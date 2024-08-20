f167aa0492160b88ff0dcb09044c0293160c63e8
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2024-08-20 18:00:19 +0530

    committing my fist service to integarte with jenkins

---
+++ UserService/UserService/src/main/java/com/user/service/entities/User.java
@@ -0,0 +1,36 @@
+package com.user.service.entities;
+
+import com.user.service.entities.Rating;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "microservice_users")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+    @Column(name = "EMAIL")
+
+    private String email;
+    @Column(name = "ABOUT")
+    private String about;
+    //other user properties that you want
+
+    @Transient
+    private List<Rating> ratings=new ArrayList<>();
+
+
+}
